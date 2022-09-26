9c4ad299fe20510a995d03f8ed01c09b9205ab0c
Martin Craig <martin.craig@nottingham.ac.uk>
2022-09-26 13:52:59 +0100

    Renamed for consistency with rest

--- BRC_qc/scripts/idpqc.py
+++ BRC_qc/scripts/idpqc.py
@@ -8,8 +8,9 @@ import pandas as pd
 
 GLOBAL_IDPLIST_PATH = "config/IDP_list.txt"
 SUBJECT_IDPS_PATH = "analysis/IDP_files/IDPs.txt"
+IDP_NAMES = None
 
-def get_idp_names():
+def _get_idp_names():
     idp_names_file = os.path.join(os.environ["BRC_GLOBAL_DIR"], GLOBAL_IDPLIST_PATH)
     idp_names = pd.read_csv(idp_names_file, sep="\s+", header=None)
     idp_names = list(idp_names[1])
@@ -18,23 +19,22 @@ def get_idp_names():
     print(" - Found %i IDP names" % len(idp_names))
     return idp_names
 
-def subject_to_json(subjdir, idp_names, out_path):
+def run_idpqc(subjid, subjdir, out_fpath):
+    print(f" - Running IDP QC in {subjdir}")
+    if IDP_NAMES is not None:
+        IDP_NAMES = _get_idp_names()
     subj_idps_file = os.path.join(subjdir, SUBJECT_IDPS_PATH)
     with open(subj_idps_file, "r") as f:
         idps = f.read().split()[1:]
     print(f" - Found {len(idps)} IDPs for subject")
 
     qc_data = {}
-    for idp, name in zip(idps, idp_names):
+    for idp, name in zip(idps, IDP_NAMES):
         if not idp.strip().lower() == "nan":
             qc_data[f"qc_{name}"] = float(idp)
 
-    os.makedirs(os.path.dirname(out_path), exist_ok=True)
-    with open(out_path, 'w') as f:
+    out_fpath = os.path.join(subjdir, out_fpath)
+    os.makedirs(os.path.dirname(out_fpath), exist_ok=True)
+    with open(out_fpath, 'w') as f:
         json.dump(qc_data, f, indent=4)
-    print(f" - Wrote subject QC output to {out_path}")
-
-def main():
-    idp_names = get_idp_names()
-    for subjdir in sys.argv[1:]:
-        do_subject(subjdir, idp_names, "qc.json")
+    print(f" - Wrote subject QC output to {out_fpath}")
